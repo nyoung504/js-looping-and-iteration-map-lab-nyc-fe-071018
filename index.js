2a5a87f090facb4f971835ef43bf7858958eb169
Nicole Young <coley504@gmail.com>
2018-09-03 16:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution in this file.
+const lowerCaseDrivers = ['DALE', 'CHIP', 'DANICA']
+const nameToAttributes = ['Victoria Secret', 'Danica Patrick', 'Dale Earnhart']
+attributesToPhrase
