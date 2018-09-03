33352a0ca23d539d1766ce0d61449a770bddb287
Nicole Young <coley504@gmail.com>
2018-09-03 16:08:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file.
 const lowerCaseDrivers = ['DALE', 'CHIP', 'DANICA']
 const nameToAttributes = ['Victoria Secret', 'Danica Patrick', 'Dale Earnhart']
-attributesToPhrase
+let attributesToPhrase ="<NAME> is from <HOMETOWN>"
+
